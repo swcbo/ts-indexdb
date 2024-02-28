a5cdaf7ab8f5b057e32cc7f4897ecd3fd15178f9
jack <543612779@qq.com>
2024-02-28 21:38:45 +0800

    fix: v8 not support run coverage test on browser

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,36 +1,40 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, afterEach } from 'vitest';
 import { getInstance, init } from '../libs';
 
+const options = { name: 'testDB', version: 1, tables: [] }; // 示例选项
+
 describe('IndexDB Singleton', () => {
-	const options = { name: 'testDB', version: 1, tables: [] }; // 示例选项
+	afterEach(() => {
+		// 确保每次执行用例前, 都回归为初始状态
+		try {
+			const instance = getInstance();
+			instance.close();
+		} catch {
+			console.log('instance existed');
+		}
+	});
 
 	it('should create an IndexDB instance with init', async () => {
 		const dbInstance = await init(options);
 		expect(dbInstance).toBeDefined();
-		// 间接测试通过公共方法或观察行为
 	});
-
+	// 确保之前没有初始化实例
+	// 此步骤可能需要根据你的实际实现调整，以确保实例未初始化
 	it('should throw an error when getInstance is called without initialization', () => {
-		const a = getInstance();
-		console.log('a',a)
-		// 确保之前没有初始化实例
-		// 此步骤可能需要根据你的实际实现调整，以确保实例未初始化
 		expect(() => getInstance()).toThrow(
 			'IndexDB instance not initialized. Call init() first.',
 		);
 	});
-
+	// 多次init后，实例应该依旧为第一次初始化时产生的
 	it('should return the same instance on subsequent calls to init', async () => {
 		const firstInstance = await init(options);
 		const secondInstance = await init(options);
 		expect(firstInstance).toBe(secondInstance);
 	});
-
-	// 如果有可能验证实例状态的公开方法，可以添加更多测试
-	// 例如，假设有一个方法可以检查数据库是否打开
+	// 初始化后，根据 indexDB instance 的 connect status 确认是否 已完成数据库连接
 	it('should indicate the database is open after initialization', async () => {
-	  await init(options);
-      const instance = getInstance()
-	  expect(instance.getConnectStatus()).toBeTruthy();
+		await init(options);
+		const instance = getInstance();
+		expect(instance.getConnectStatus()).toBeTruthy();
 	});
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -2,10 +2,15 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	test: {
+		reporters: ['default', 'html'],
 		browser: {
 			name: 'chrome',
 			headless: true,
 			enabled: true,
+		},
+		coverage: {
+			enabled: true,
+			provider: 'istanbul',
 		},
 	},
 });
